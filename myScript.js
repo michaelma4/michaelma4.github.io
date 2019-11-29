36f6308213ee4ce973c41339bc9cb0f7dd70a0d2
michaelma4 <>
2019-11-28 21:05:59 -0500

    localsotrage testing

--- myScript.js
+++ myScript.js
@@ -7,3 +7,16 @@ function onClick() {
         clicks += 1;
         document.getElementById("clicks").innerHTML = clicks;
     };
+	
+function clickCounter() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
