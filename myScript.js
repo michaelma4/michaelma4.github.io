bbe81071763b401c8be8e5306b060d74e44b384c
michaelma4 <>
2019-11-28 19:42:27 -0500

    click test

--- myScript.js
+++ myScript.js
@@ -1,3 +1,9 @@
 function myFunction() {
  document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
+
+var clicks = 0;
+function onClick() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
